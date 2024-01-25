161f240a3badeb1f6c4e15ad8da387696ac98748
HeroKnightG <asayhero69@gmail.com>
2024-01-25 19:22:48 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <>
+     <div className="Main">
+        <div className="Header">
+          Login
+        </div>
+        <div className="input">
+          <input type="text" className="btn" placeholder="Username"></input>
+        </div>
+        <div className="input">
+          <input type="password" className="btn" placeholder="Password"></input>
+        </div>
+        <div className="End">
+          <button className="Button">Login</button>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default App
